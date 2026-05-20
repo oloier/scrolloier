faf604fde4eb582c68b609d439d6823c1e59f176
Scott Oloier <scottoloier@gmail.com>
2026-05-20 12:50:36 -0500

    rss: cdata descriptions, youtube/vimeo embeds, video blob support

--- rss.php
+++ rss.php
@@ -18,24 +18,37 @@ echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
     $id      = (int) $row['id'];
     $link    = $base . '?post=' . $id;
     $pubDate = date(DATE_RSS, strtotime($row['date']));
+    $url     = $row['url'] ?? '';
     $desc    = '';
+
     if (!empty($row['has_image'])) {
         $src   = $base . 'img.php?id=' . $id;
-        $desc .= '<img src="' . htmlspecialchars($src) . '" />';
-    } elseif (!empty($row['url'])) {
-        $u     = htmlspecialchars($row['url']);
-        $desc .= '<a href="' . $u . '">' . $u . '</a>';
+        $mime  = $row['mime'] ?? '';
+        if (strpos($mime, 'video/') === 0) {
+            $desc .= '<video controls><source src="' . $src . '" type="' . htmlspecialchars($mime) . '"></video>';
+        } else {
+            $desc .= '<img src="' . $src . '" style="max-width:100%" />';
+        }
+    } elseif (!empty($url)) {
+        if (preg_match('/(?:youtube\.com\/watch\?.*v=|youtu\.be\/|youtube\.com\/embed\/)([a-zA-Z0-9_-]{11})/', $url, $m)) {
+            $desc .= '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $m[1] . '" frameborder="0" allowfullscreen></iframe>';
+        } elseif (preg_match('/vimeo\.com\/(\d+)/', $url, $m)) {
+            $desc .= '<iframe src="https://player.vimeo.com/video/' . $m[1] . '" width="560" height="315" frameborder="0" allowfullscreen></iframe>';
+        } else {
+            $u = htmlspecialchars($url);
+            $desc .= '<a href="' . $u . '">' . $u . '</a>';
+        }
     }
+
     foreach ($agg->getPostComments($id) as $c) {
-        $cName = htmlspecialchars($c['name']);
-        $desc .= '<blockquote><b>' . $cName . '</b><p>' . $c['comment'] . '</p></blockquote>';
+        $desc .= '<blockquote><b>' . htmlspecialchars($c['name']) . '</b><p>' . $c['comment'] . '</p></blockquote>';
     }
 ?>    <item>
-        <title><?= htmlspecialchars($row['title'] ?? '') ?></title>
+        <title><?= htmlspecialchars($row['title'] ?? 'untitled') ?></title>
         <link><?= htmlspecialchars($link) ?></link>
         <guid><?= htmlspecialchars($link) ?></guid>
         <pubDate><?= $pubDate ?></pubDate>
-        <description><?= htmlspecialchars($desc) ?></description>
+        <description><![CDATA[<?= $desc ?>]]></description>
     </item>
 <?php endforeach ?>
 </channel>
