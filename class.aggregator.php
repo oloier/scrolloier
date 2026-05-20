1e3f73e9848225ffd1eae37c0050c92290bb94f3
Scott Oloier <scottoloier@gmail.com>
2026-05-20 10:36:53 -0500

    inline embed(), drop class.embedly.php

--- class.aggregator.php
+++ class.aggregator.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once("class.db.php");
-require_once("class.embedly.php");
 
 define('APP_PATH',    '/share/');
 define('ASSETS_PATH', APP_PATH . 'assets/');
@@ -240,4 +239,34 @@ class aggregator
                 </dd>
             </dl>";
     }
+}
+
+function embed($url)
+{
+    if (empty($url)) return '';
+
+    if (preg_match('/https?:\/\/\S+\.(?:png|jpg|jpeg|gif|svg)(\?[^\s]*)?$/i', $url)) {
+        $safe = htmlspecialchars($url, ENT_QUOTES);
+        return '<a class="loader" href="' . $safe . '" rel="lightbox"><img src="' . $safe . '" alt="" loading="lazy" /></a>';
+    }
+
+    if (preg_match('/https?:\/\/\S+\.(?:mp4|webm|ogg)(\?[^\s]*)?$/i', $url)) {
+        $safe = htmlspecialchars($url, ENT_QUOTES);
+        return '<video controls><source src="' . $safe . '"></video>';
+    }
+
+    if (preg_match('/(?:youtube\.com\/watch\?.*v=|youtu\.be\/|youtube\.com\/embed\/)([a-zA-Z0-9_-]{11})/', $url, $m)) {
+        $id    = $m[1];
+        $thumb = 'https://img.youtube.com/vi/' . $id . '/hqdefault.jpg';
+        $embed = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $id . '?autoplay=1" frameborder="0" allowfullscreen></iframe>';
+        return '<img src="' . $thumb . '" alt="" data-embed="' . htmlspecialchars($embed, ENT_QUOTES) . '" loading="lazy" /><a>&#9654;</a>';
+    }
+
+    if (preg_match('/vimeo\.com\/(\d+)/', $url, $m)) {
+        $safe  = htmlspecialchars($url, ENT_QUOTES);
+        return '<a href="' . $safe . '" class="regular">&#9654; Vimeo</a>';
+    }
+
+    $safe = htmlspecialchars($url, ENT_QUOTES);
+    return '<a href="' . $safe . '" class="regular">' . htmlspecialchars($url) . '</a>';
 }
