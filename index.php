f43d8e68150cbc3e8e8223aff3442603fb487eb3
Scott Oloier <scottoloier@gmail.com>
2026-05-21 11:15:30 -0500

    change header font to oswald

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ $pageTitle = 'scrolloier' . ($routeUser ? ' / ' . $routeUser : '');
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?= ASSETS_PATH ?>style.css" />
     <link rel="icon" type="image/svg+xml" href="<?= ASSETS_PATH ?>img/logo.svg" />
     <link rel="alternate" type="application/rss+xml" title="<?= htmlspecialchars($pageTitle) ?>" href="<?= htmlspecialchars($feedHref) ?>" />
