ce490b603b821b6c464ab16d880aaf115a06841b
Scott Oloier <scottoloier@gmail.com>
2026-05-22 16:47:33 -0500

    do the thing for web browser theming / window UI colors

--- index.php
+++ index.php
@@ -80,6 +80,8 @@ $pageTitle = 'scrolloier' . ($routeUser ? ' / ' . $routeUser : '');
     <meta charset="utf-8" />
     <meta name="robots" content="noindex,nofollow" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="color-scheme" content="dark" />
+    <meta name="theme-color" content="#111111" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400&display=swap" rel="stylesheet">
